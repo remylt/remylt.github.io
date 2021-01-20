b731ba7c2e16287b55a680be263e3fc6db62fc0b
remylt <remy.laot@gmail.com>
2021-01-20 14:18:45 +0100

    Updates

---
+++ precache-manifest.5627df59c532ba83a96db379b0966f3f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287f9a8c7c1d031f697158f3ef2dcc84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51e06851c0bfa98afa2e",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "f5061c15a4596a03dd82",
+    "url": "/static/css/4.7bf9d2d9.chunk.css"
+  },
+  {
+    "revision": "1d5d6d370a4aa481066d",
+    "url": "/static/css/main.398a15b9.chunk.css"
+  },
+  {
+    "revision": "3c6b1001642a35e95da7",
+    "url": "/static/js/2.41968d9b.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.41968d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e06851c0bfa98afa2e",
+    "url": "/static/js/3.f98292aa.chunk.js"
+  },
+  {
+    "revision": "f5061c15a4596a03dd82",
+    "url": "/static/js/4.093eec8e.chunk.js"
+  },
+  {
+    "revision": "1d5d6d370a4aa481066d",
+    "url": "/static/js/main.4444bb02.chunk.js"
+  },
+  {
+    "revision": "67ad630cd0f8b15dfa2c",
+    "url": "/static/js/runtime-main.742a8b97.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9448d85a1e6a3efcef4cf9ec3e6aeb23",
+    "url": "/static/media/api.9448d85a.png"
+  },
+  {
+    "revision": "352bd593c0db5d770040afc53dc2dd48",
+    "url": "/static/media/bts_logo.352bd593.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "97d08d6e758563c2637aa93c607d603c",
+    "url": "/static/media/getup.97d08d6e.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "898f0562eacc24ea7b8f8878c77aa8ca",
+    "url": "/static/media/iut_logo.898f0562.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a240ac0c891bdc356c9bdc5482532017",
+    "url": "/static/media/talksCardBack.a240ac0c.svg"
+  }
+]);
